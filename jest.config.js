8b22284c7acf20b2312ea51d00e2618fd5520c4c
Asher Cohen <asherlc@asherlc.com>
2023-03-29 08:15:32 -0700

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ import baseConfig from "./jest.config.base.js";
 export default {
     ...baseConfig,
     projects: ["<rootDir>", "<rootDir>/packages/*"],
+    testPathIgnorePatterns: ["<rootDir>/tests/integration", "<rootDir>/dist", "<rootDir>/packages/.+/dist"]
 };

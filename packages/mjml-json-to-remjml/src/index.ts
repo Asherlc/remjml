26ce95d3638a1ede7526a2b5a22ced72981692b4
Asher Cohen <asherlc@asherlc.com>
2023-10-02 15:41:20 -0700

    undo

--- packages/mjml-json-to-remjml/src/index.ts
+++ packages/mjml-json-to-remjml/src/index.ts
@@ -8,16 +8,16 @@ export default function mjmlJsonToRemjml(
 ): MjmlAstNode {
   let childNodes: MjmlAstNode[] | undefined;
   if (isParentComponent(mjmlJson)) {
-    //   childNodes = mjmlJson.children?.map(mjmlJsonToRemjml);
+    childNodes = mjmlJson.children?.map(mjmlJsonToRemjml);
   }
-  // const node: MjmlAstNode = omitBy(
-  //   {
-  //     type: mjmlJson.tagName,
-  //     attributes: mjmlJson.attributes,
-  //     children: childNodes,
-  //     content: isEndComponent(mjmlJson) ? mjmlJson.content : undefined,
-  //   },
-  //   isUndefined
-  // ) as unknown as MjmlAstNode;
-  // return node;
+  const node: MjmlAstNode = omitBy(
+    {
+      type: mjmlJson.tagName,
+      attributes: mjmlJson.attributes,
+      children: childNodes,
+      content: isEndComponent(mjmlJson) ? mjmlJson.content : undefined,
+    },
+    isUndefined
+  ) as unknown as MjmlAstNode;
+  return node;
 }

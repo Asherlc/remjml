baf00e2a6819e5d2896a31be0911592be13fb073
Asher Cohen <asherlc@asherlc.com>
2023-10-03 14:15:26 -0700

    add hot reloading

--- scripts/debug-fixture.ts
+++ scripts/debug-fixture.ts
@@ -1,14 +1,46 @@
-#!/usr/bin/env node --loader ts-node/esm
 import { program, Option } from "commander";
-import path from "path";
-import fsPromise from "fs/promises";
+import path from "node:path";
+import { readFile, watch } from "node:fs/promises";
 
+import type { Browser, Page } from "puppeteer";
 import puppeteer from "puppeteer";
 
 import { unified } from "unified";
 import remjmlParse from "remjml-parse";
 import remjmlRehype from "remjml-rehype";
 import rehypeStringify from "rehype-stringify";
+
+async function mjmlToBase64UrlForFixtureName(emailFixtureName) {
+  const emailFixtureDirectoryPath: string = path.resolve(
+    "./tests/fixtures/mjml-emails/"
+  );
+  const mjmlBuffer = await readFile(
+    path.format({
+      dir: emailFixtureDirectoryPath,
+      name: emailFixtureName,
+      ext: ".mjml",
+    })
+  );
+
+  const mjml: string = mjmlBuffer.toString();
+
+  const html: string = (
+    await unified()
+      .use(remjmlParse)
+      .use(remjmlRehype)
+      .use(rehypeStringify, {
+        allowDangerousHtml: true,
+      })
+      .process(mjml)
+  ).value.toString();
+
+  const buffer: Buffer = Buffer.from(html);
+
+  const base64Url: string = `data:text/html;base64,${buffer.toString(
+    "base64"
+  )}`;
+  return base64Url;
+}
 
 interface Options {
   fixtureName: string;
@@ -24,45 +56,28 @@ const emailFixtureNames = [
   "worldly",
 ];
 
-const fixtureNameOption = new Option("--fixture-name <fixture-name>")
+const fixtureNameOption: Option = new Option("--fixture-name <fixture-name>")
   .choices(emailFixtureNames)
   .makeOptionMandatory();
 
-program.addOption(fixtureNameOption);
+program.addOption(fixtureNameOption).option("port", "server port", "9734");
 
 program.parse();
 
-const emailFixtureDirectoryPath = path.resolve("./tests/fixtures/mjml-emails/");
+const emailFixtureName: string = program.opts<Options>().fixtureName;
 
-const emailFixtureName = program.opts<Options>().fixtureName;
+const base64Url: string = await mjmlToBase64UrlForFixtureName(emailFixtureName);
 
-const mjmlBuffer = await fsPromise.readFile(
-  path.format({
-    dir: emailFixtureDirectoryPath,
-    name: emailFixtureName,
-    ext: ".mjml",
-  })
-);
-
-const mjml: string = mjmlBuffer.toString();
-
-const html: string = (
-  await unified()
-    .use(remjmlParse)
-    .use(remjmlRehype)
-    .use(rehypeStringify, {
-      allowDangerousHtml: true,
-    })
-    .process(mjml)
-).value.toString();
-
-const buffer: Buffer = Buffer.from(html);
-
-const base64Url: string = `data:text/html;base64,${buffer.toString("base64")}`;
-
-const browser = await puppeteer.launch({
+const browser: Browser = await puppeteer.launch({
   headless: false,
   args: ["--disable-notifications"],
 });
-const page = await browser.newPage();
+const page: Page = await browser.newPage();
 page.goto(base64Url);
+
+const watcher = watch(".", { recursive: true });
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+for await (const event of watcher) {
+  page.goto(await mjmlToBase64UrlForFixtureName(emailFixtureName));
+}
